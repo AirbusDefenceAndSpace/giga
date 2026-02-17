45b86aa35ae3121b426c40de028fd86c0694a89b
PatrickMartin1804 <patrick.martin1804@gmail.com>
2026-02-17 12:02:12 +0100

    Fix: Added an Error Message for avgpool layers, since they are not currently supported in the converter

--- giga/python/src/GIGA_export/nnef_to_giga.py
+++ giga/python/src/GIGA_export/nnef_to_giga.py
@@ -336,6 +336,10 @@ class GIGA_Code_Generator:
             # TODO: support dense layers
             if operation.name in ['linear', 'matmul']:
                 raise NotImplementedError("Dense and linear layers are not yet supported")
+            
+            if operation.name == "avg_pool":
+                raise NotImplementedError("Avg_pool is not yet supported")
+
 
             # We assume 'relu' as already processed as part of a previous layer (either conv or dense)
             if operation.name == 'relu':
